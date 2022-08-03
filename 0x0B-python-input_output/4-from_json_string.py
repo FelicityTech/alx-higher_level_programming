d69486923e11f52e6c466795d999397fc9d710c6
Solomon Eniola Adegoke <souceking@gmail.com>
2022-08-03 04:59:06 +0000

    output-json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/python3
+"""
+Module for from_json_string method.
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object represented by a JSON string.
+    """
+    return (json.loads(my_str))
