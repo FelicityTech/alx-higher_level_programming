055f4c1ef6ac0df9dbc9488e78725f7f62a2fad2
Solomon Eniola Adegoke <souceking@gmail.com>
2022-09-09 03:09:29 -0700

    Create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Write a script that create the database htbn_0c_0 in your MySQL server.
+-- if ther database htbn_0c_0 already exists, your script should not fail
+BREATE DATABASE IF NOT EXISTS htbn_0c_0;
