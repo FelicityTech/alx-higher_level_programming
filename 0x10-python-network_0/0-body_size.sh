35ffc54f477a352a2fd468727ae32ddb96e66ef4
Solomon Eniola Adegoke <souceking@gmail.com>
2023-01-12 20:13:55 -0800

    network file (script)

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -so /dev/null -w '%{size_download}\n' "$1"
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script takes in a URL and displays all HTTP methods the server will accept
+curl -sI -X OPTIONS "$1" | grep 'Allow' | cut -c8-
